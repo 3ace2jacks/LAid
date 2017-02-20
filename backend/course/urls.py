05c1a54fc8d81c2c8b67699f4bedd30a6e99a794
Ole Alexander Hole <ole@oahole.com>
2017-02-20 13:44:58 +0100

    Refactored courses and lectures
    Changed urls for resources and the way to access the API.

--- backend/course/urls.py
+++ backend/course/urls.py
@@ -4,7 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.CourseList.as_view()),
-    url(r'^lectures/$', views.LectureList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.CourseDetail.as_view()),
+    url(r'^(?P<pk>[0-9]+)/lectures/$', views.LectureList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
