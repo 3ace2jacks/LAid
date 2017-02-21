62552964e05e01a36ab6b4f389b0d300bd717f3a
Edvard Bakken <Edvard Bakken>
2017-02-21 15:22:11 +0100

    innloggingside med brukernavn og pasoord. Snakker ikke med Jkke med JWT enda

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './auth/home/index';
+import { User } from './auth/_models/index';
+import { UserService } from './auth/_services/index';
+import { AuthenticationService } from './auth/_services/index';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Courses';
+    user = 'edvard';
+    users: User[] = [];
+
+    ngOnInit() {
+        // get users from secure api end point
+    }
 }
