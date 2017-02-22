c423160f8c2e8b2825ae687acba5f9f21563809a
Edvard Bakken <Edvard Bakken>
2017-02-22 15:45:05 +0100

    innlogging med first_name & last_name

--- frontend/src/app/auth/_guards/auth.guard.ts
+++ frontend/src/app/auth/_guards/auth.guard.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
+import { AuthenticationService } from '../_services/index';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(private auth: AuthenticationService, private router: Router) { }
 
     canActivate() {
+
         if (localStorage.getItem('currentUser')) {
-            // logged in so return true
+            console.log("logged in");
             return true;
         }
 
