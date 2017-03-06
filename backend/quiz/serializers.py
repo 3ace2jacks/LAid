059f70509a2ee9768cfd5a60297cdcd8ece6a8e9
Ole Alexander Hole <ole@oahole.com>
2017-03-06 11:46:47 +0100

    Add ability to create a new quiz through the API

--- backend/quiz/serializers.py
+++ backend/quiz/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from quiz.models import Quiz, Question, Option
-
+from course.models import Lecture
 
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,9 +14,41 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = ['question', 'answer_description', 'options']
 
-class QuizStudentSerializer(serializers.ModelSerializer):
+class QuizSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True)
+    lectureID = serializers.IntegerField(write_only=True)
+    lectureQuiz = serializers.ChoiceField(choices=(('pre_quiz', 'pre_quiz'), ('post_quiz', 'post_quiz')), write_only=True)
     class Meta:
         model = Quiz
-        fields = ['id', 'title', 'start_time', 'end_time', 'questions']
+        fields = ['id', 'title', 'description', 'start_time', 'end_time', 'questions', 'lectureID', 'lectureQuiz']
+
+    def create(self, validated_data):
+        questions_data = validated_data.pop('questions')
+        lecture = Lecture.objects.get(id=validated_data.pop('lectureID'))
+        lectureQuiz = validated_data.pop('lectureQuiz')
+        quiz = Quiz.objects.create(**validated_data)
+        for question_data in questions_data:
+            print(question_data)
+            options_data = question_data.pop('options')
+            print(options_data)
+            question = Question.objects.create(quiz=quiz, **question_data)
+            for option_data in options_data:
+                Option.objects.create(question=question, **option_data)
+
+        if lectureQuiz=='post_quiz':
+            lecture.post_quiz = quiz
+        else:
+            lecture.pre_quiz = quiz
+        lecture.save()
+
+        return quiz
+
+
+
+
+
+
+
+
+
 
--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from quiz.models import Quiz
 from rest_framework import generics
-from quiz.serializers import QuizStudentSerializer
+from quiz.serializers import QuizSerializer
 
 
 
 class QuizList(generics.ListCreateAPIView):
     queryset = Quiz.objects.all()
-    serializer_class = QuizStudentSerializer
+    serializer_class = QuizSerializer
 
 
 class QuizStudentDetail(generics.RetrieveAPIView):
     queryset = Quiz.objects.all()
-    serializer_class = QuizStudentSerializer
+    serializer_class = QuizSerializer
