03c1a28b201005bbbf8a20fb0a5b46516166f9bf
Ole Alexander Hole <ole@oahole.com>
2017-03-13 17:07:12 +0100

    Add answer API view

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -5,6 +5,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^$', views.QuizList.as_view(), name='quizzes'),
     url(r'^(?P<pk>[0-9]+)/$', views.QuizStudentDetail.as_view(), name='quizDetail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.QuizStudentDetail.as_view(), name='quizDetail'),
+    url(r'^(?P<pk>[0-9]+)/answers/$', views.AnswerQuiz.as_view(), name='quizAnswer'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
