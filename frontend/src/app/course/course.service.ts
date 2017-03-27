516f17e518be7870e30e2bf2499ea37a849ca641
Ole Alexander Hole <ole@oahole.com>
2017-03-27 12:53:54 +0200

    Changed the course service to use auto auth http
    Removed the duplicate headers earlier in the headers.

--- frontend/src/app/course/course.service.ts
+++ frontend/src/app/course/course.service.ts
@@ -1,83 +1,56 @@
-import { Injectable, OnChanges } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Course, Lecture } from './models';
-import { Headers, Http, Response} from '@angular/http';
-import { AuthenticationService } from '../auth/_services';
-import { Observable } from 'rxjs';
-import { apiUrl } from '../local-settings';
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/map';
+import { AuthHttpService } from '../auth/auth-http.service';
 
 
 @Injectable()
 export class CourseService {
 
-    private courseUrl = apiUrl + '/courses/';
-    private headers:Headers;
+    constructor(private authHttp: AuthHttpService) { }
 
-    constructor(private http: Http, private authenticationService:AuthenticationService) {
-        this.updateHeaders();
+    getOwnCourses(): Promise<Course[]> {
+        return this.authHttp.get('/courses/member/')
+            .toPromise()
+            .then(response => response.json() as Course[])
+            .catch(error => error.json());
     }
 
-    updateHeaders() {
-        console.log("Token changed");
-        this.headers = new Headers();
-        this.headers.append('Content-Type', 'application/json');
-        this.headers.append('Authorization', 'JWT ' + this.authenticationService.getToken());
+    getAvailableCourses(): Promise<Course[]> {
+        return this.authHttp.get('/courses/available/')
+            .toPromise()
+            .then(response => response.json() as Course[])
+            .catch(error => error.json());
     }
 
-    joinCourse(course:Course) : Observable<boolean> {
-        return this.http.post(this.courseUrl + course.id + "/join/", JSON.stringify({}), { headers : this.headers }).
-        map((response: Response) => {
-            return true;
-        })
+    getCourse(id: number): Promise<Course> {
+        return this.authHttp.get('/courses/' + id + '/')
+            .toPromise()
+            .then(response => response.json() as Course)
+            .catch(error => error.json());
     }
 
-    getAllCourses(): Promise<Course[]> {
-        return this.http.get(this.courseUrl + 'all' + '.json', {headers : this.headers} )
-        .toPromise().then(response => response.json() as Course[])
-        .catch(this.handleError);
+    join(id: number): Promise<void> {
+        return this.authHttp.post('/courses/' + id + '/join/', {})
+            .toPromise()
+            .then(response => { })
+            .catch(error => error.json());
     }
 
-    getCourses(): Promise<Course[]> {
-        return this.http.get(this.courseUrl + '.json', {headers : this.headers} )
-        .toPromise().
-        then(response => response.json() as Course[])
-        .catch(this.handleError);
-    }
-
-    getCourse(id:number): Promise<Course> {
-        return this.http.get(this.courseUrl + id + '.json', {headers : this.headers})
-        .toPromise().then(response => response.json() as Course)
-        .catch(this.handleError);
-    }
-
-    getLectures(id:number): Promise<Lecture[]> {
-        return this.http.get(this.courseUrl + id + '/lectures' + '.json', {headers : this.headers})
-        .toPromise()
-        .then(response => response.json() as Lecture[])
-        .catch(this.handleError);
-    }
-
-    addCourse(code: string, name: string, year: number, term: string) : Promise<void> {
-        return this.http.post(this.courseUrl, JSON.stringify({code: code, name: name, year: year, term: term}), { headers : this.headers })
-        .toPromise()
-        .then(() => null)
-        .catch(this.handleError);
-    }
-
-    private handleError(error: any): Promise<any> {
-        return Promise.reject(error.message || error);
-    }
-
-    getTopCourses(number:number): Promise<Course[]> {
-        return this.getCourses().then(courses => courses.slice(0, number));
+    getLectures(courseID: number): Promise<Lecture[]> {
+        return this.authHttp.get('/courses/' + courseID + '/lectures/')
+            .toPromise()
+            .then(response => response.json() as Lecture[])
+            .catch(error => error.json());
     }
 
     getLecture(id: number): Promise<Lecture> {
-        let Url = apiUrl + "/courses/lecture/" + id;
-        console.log(Url);
-        return this.http.get(Url + '.json', {headers : this.headers})
-        .toPromise().then(response => response.json() as Lecture)
-        .catch(this.handleError);
+        return this.authHttp.get('/lectures/' + id + '/')
+            .toPromise()
+            .then(response => response.json() as Lecture)
+            .catch(error => error.json());
+    }
+
+    createCourse(course: Course) {
+        
     }
 }
--- frontend/src/app/course/models.ts
+++ frontend/src/app/course/models.ts
@@ -1,6 +1,9 @@
+
 export class Lecture {
     id: number;
+    course: number;
     title: string;
+    date: string;
     start_time: string;
     end_time: string;
     pre_quiz: number;
@@ -9,9 +12,9 @@ export class Lecture {
 
 export class Course {
     id: number;
-    staff:boolean;
     code: string;
     name: string;
     year: number;
     term: string;
+    role: string; // The user's role in the course. Either null, staff or student
 }
