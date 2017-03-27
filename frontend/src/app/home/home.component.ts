cd9203cd5f933b99ce32919622680df991e88108
Ole Alexander Hole <ole@oahole.com>
2017-03-27 12:58:11 +0200

    Recreated the home component with ng generate

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../auth/_services';
-
-
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'home.component.html'
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
+export class HomeComponent implements OnInit {
 
-export class HomeComponent {
+  constructor() { }
 
-    constructor(private auth: AuthenticationService) { }
-
-
+  ngOnInit() {
+  }
 
 }
