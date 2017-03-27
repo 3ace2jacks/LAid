7d173fa470cfe562722414da7426491f385cdcd8
Ole Alexander Hole <ole@oahole.com>
2017-03-27 13:37:50 +0200

    App component now include the nav component.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,34 +1,10 @@
 import { Component } from '@angular/core';
-import { AuthenticationService } from './auth/_services';
-import { Router } from '@angular/router';
-
-// declare var Pusher: any;
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    // private pusher: any;
-    // private channel: any;
-
-    constructor(
-        private auth:AuthenticationService,
-        private router: Router) {
-
-            // this.pusher = new Pusher('24e1ae04e2c67d323ec5', {
-            //     cluster: 'eu',
-            //     encrypted: true,
-            // });
-            // this.channel = this.pusher.subscribe('my-channel');
-            // this.channel.bind('my-event', function(data) {
-            //     alert(data.message);
-            // })
-        }
-
-
-    logout() {
-        this.auth.logout();
-        this.router.navigate(['/']);
-    }
+  title = 'app works!';
 }
